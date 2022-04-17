698a9ed68ac3e4aac9b8bf86fe064dd81b377d33
akpalueyram <meakpalu002@st.ug.edu.gh>
2022-04-17 10:21:48 +0000

    quick update
    c++ code

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int b;
+    int h;
+    cout<<"Enter the base of triangle: ";
+    cin>>b;
+    cout<<"Enter the height of triangle: ";
+    cin>>b;
+    float area = 0.5 * b * a ;
+        cout<<"The area of triangle is : "<<area<< endl;
+    return 0;
+}
